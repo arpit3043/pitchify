9bbb72a701e090a06aa6f65cab28e1663f60a050
onlyVishesh <visheshmaurya14@gmail.com>
2025-01-06 17:55:12 +0530

    feature: follow/unfollow routes created
    Added Follow and unfollow routes to the server to test route use /api/users/:id/follow or /api/users/:id/unfollow as end points

--- src/modules/user/controllers/userControllers.js
+++ src/modules/user/controllers/userControllers.js
@@ -37,9 +37,150 @@ const viewUser = async (req, res) => {
 };
 
 // Follow User
-const followUser = async (req, res) => {};
+const followUser = async (req, res) => {
+  try {
+    const loggedInUser = req.user;
+    const { id } = req.params;
+    if (!loggedInUser) {
+      return res.status(401).json({
+        success: false,
+        message: "Unauthorized. Please log in again.",
+      });
+    }
+
+    if (!id) {
+      return res.status(400).json({
+        success: false,
+        message: "Please provide a user ID.",
+      });
+    }
+
+    if (!mongoose.Types.ObjectId.isValid(id)) {
+      return res.status(400).json({
+        success: false,
+        message: "Invalid user ID.",
+      });
+    }
+
+    const targetUser = await User.findById(id);
+    if (!targetUser) {
+      return res.status(404).json({
+        success: false,
+        message: "User not found.",
+      });
+    }
+
+    if (loggedInUser._id.equals(id)) {
+      return res.status(400).json({
+        success: false,
+        message: "You Could not follow yourself",
+      });
+    }
+
+    const isFollowing = loggedInUser.following.some((userId) =>
+      userId.equals(id)
+    );
+
+    if (isFollowing) {
+      return res.status(400).json({
+        success: false,
+        message: `You are already Following ${
+          targetUser.name[0].toUpperCase() + targetUser.name.slice(1)
+        }`,
+      });
+    }
+    loggedInUser.following.unshift(id);
+    targetUser.followers.unshift(loggedInUser._id);
+    await loggedInUser.save();
+    await targetUser.save();
+
+    return res.status(200).json({
+      success: true,
+      message: `You are Following ${
+        targetUser.name[0].toUpperCase() + targetUser.name.slice(1)
+      }`,
+    });
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ success: false, message: err.message });
+  }
+};
 
 // Unfollow User
-const unfollowUser = (req, res) => {};
+const unfollowUser = async (req, res) => {
+  try {
+    const loggedInUser = req.user;
+    const { id } = req.params;
+
+    if (!loggedInUser) {
+      return res.status(401).json({
+        success: false,
+        message: "Unauthorized. Please log in again.",
+      });
+    }
+
+    if (!id) {
+      return res.status(400).json({
+        success: false,
+        message: "Please provide a user ID.",
+      });
+    }
+
+    if (!mongoose.Types.ObjectId.isValid(id)) {
+      return res.status(400).json({
+        success: false,
+        message: "Invalid user ID.",
+      });
+    }
+
+    const targetUser = await User.findById(id);
+    if (!targetUser) {
+      return res.status(404).json({
+        success: false,
+        message: "User not found.",
+      });
+    }
+
+    if (loggedInUser._id.equals(id)) {
+      return res.status(400).json({
+        success: false,
+        message: "You cannot unfollow yourself.",
+      });
+    }
+
+    const isFollowing = loggedInUser.following.some((userId) =>
+      userId.equals(id)
+    );
+    if (!isFollowing) {
+      return res.status(400).json({
+        success: false,
+        message: `You are not following ${targetUser.name}.`,
+      });
+    }
+
+    loggedInUser.following = loggedInUser.following.filter(
+      (userId) => !userId.equals(id)
+    );
+
+    targetUser.followers = targetUser.followers.filter(
+      (userId) => !userId.equals(loggedInUser._id)
+    );
+
+    await loggedInUser.save();
+    await targetUser.save();
+
+    return res.status(200).json({
+      success: true,
+      message: `You have unfollowed ${targetUser.name}.`,
+    });
+  } catch (err) {
+    console.error(err);
+    return res.status(500).json({
+      success: false,
+      message: "An error occurred while trying to unfollow the user.",
+      error: err.message,
+    });
+  }
+};
 
 module.exports = { viewUser, followUser, unfollowUser };
