ae590ebab6eb7fad165673774fd032fe79abd546
onlyVishesh <visheshmaurya14@gmail.com>
2025-01-08 11:30:59 +0530

    Feature:View profile
    Added view profile route ie /api/users/:id

--- src/app/routes.js
+++ src/app/routes.js
@@ -2,12 +2,13 @@ const express = require("express");
 
 const profileRoutes = require("../modules/profile/routes/founderRoutes.js");
 const authRoutes = require("../modules/auth/routes/authRoutes.js");
+const userRoutes = require("../modules/user/routes/userRoutes.js");
 const postRoutes = require("../modules/activityFeed/routes/postRoutes.js");
 
 const router = express.Router();
 
 router.use("/profile", profileRoutes);
-router.use("/users", authRoutes);
+router.use("/users", authRoutes, userRoutes);
 router.use("/post", postRoutes);
 
 module.exports = router;
